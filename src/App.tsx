0e9efb388fd4b07fbee952ad38f9ff9e12228178
david19589 <davitnasyidashvili2006@gmail.com>
2024-06-04 15:18:18 +0400

    changed style in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center">
       <Home />
     </div>
   );
